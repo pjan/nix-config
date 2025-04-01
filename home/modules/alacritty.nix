48acb626505e09182d63c7483e6ea30f46c25813
pjan <pjan@vandaele.io>
2025-04-01 18:00:37 +0900

    comment out alacritty icon update script

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -108,15 +108,15 @@ in {
 
   };
 
-  home.activation.kconfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    /usr/bin/osascript <<EOD
-      use framework "AppKit"
-      use scripting additions
-      set iconURL to current application's NSURL's URLWithString:"https://github.com/bouk/alacritty/blob/604686ba061c714f37d1db7002258517f062f0d2/extra/osx/Alacritty.app/Contents/Resources/alacritty.icns?raw=true"
-      set iconImage to (current application's NSImage's alloc)'s initWithContentsOfURL:iconURL
-      set workspace to current application's NSWorkspace's sharedWorkspace()
-      workspace's setIcon:iconImage forFile:"/Applications/Alacritty.app" options:0
-    EOD
-  '';
+  # home.activation.kconfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  #   /usr/bin/osascript <<EOD
+  #     use framework "AppKit"
+  #     use scripting additions
+  #     set iconURL to current application's NSURL's URLWithString:"https://github.com/bouk/alacritty/blob/604686ba061c714f37d1db7002258517f062f0d2/extra/osx/Alacritty.app/Contents/Resources/alacritty.icns?raw=true"
+  #     set iconImage to (current application's NSImage's alloc)'s initWithContentsOfURL:iconURL
+  #     set workspace to current application's NSWorkspace's sharedWorkspace()
+  #     workspace's setIcon:iconImage forFile:"/Applications/Alacritty.app" options:0
+  #   EOD
+  # '';
 
 }
